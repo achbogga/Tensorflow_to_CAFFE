ee3669b7cd89432db1eadf18a3f5e7b2d172c5d3
Achyut Boggaram <aboggaram@objectvideo.com>
2018-05-17 17:03:42 -0400

    Succesful weights transfer done!

--- driver.py
+++ driver.py
@@ -1,4 +1,4 @@
 from inception_resnet_v1_facenet_caffe import *
-inc_v4 = InceptionV4('train.txt', 'val.txt', 512, 9131)
+inc_v4 = InceptionV4('train.txt', 'val.txt', 128, 9131)
 with open('facenet_train_test.prototxt', 'w') as f:
-    f.write(str(inc_v4.inception_v4_proto(128)))
+    f.write(str(inc_v4.inception_v4_proto(16)))
